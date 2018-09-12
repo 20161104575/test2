30f67052fc97c4ca81d8c202c5bd188f565e40d5
20161104575 <1975950726@qq.com>
2018-09-12 11:31:35 +0800

    add

---
+++ test2/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  test2
+//
+//  Created by 20161104575 on 2018/9/7.
+//  Copyright © 2018年 20161104575. All rights reserved.
+//
+
+import Foundation
+
+
+var sum = 0
+
+for var j in 1...100 {
+    
+    sum = sum + j
+    
+}
+print(sum)
